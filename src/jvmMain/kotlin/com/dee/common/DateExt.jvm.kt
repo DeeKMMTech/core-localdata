1da90e6f97c1f674685407ce8dab6f5e1e50887c
Dee <hein.h@siriustech.io>
2024-03-23 21:04:55 +0700

    [Remove] Unused codes

--- src/jvmMain/kotlin/com/dee/common/DateExt.jvm.kt
+++
@@ -1,5 +0,0 @@
-package com.dee.common
-
-actual fun getFormattedDate(iso8601Timestamp: String, format: String): String {
-    TODO("Not yet implemented")
-}
--- src/jvmMain/kotlin/com/dee/localdata/LocalDataManager.jvm.kt
+++ src/jvmMain/kotlin/com/dee/localdata/LocalDataManager.jvm.kt
@@ -5,28 +5,28 @@ package com.dee.localdata
  */
 actual class LocalDataManager {
     actual fun getInt(key: String): Int {
-        TODO("Not yet implemented")
+       return 1
     }
 
     actual fun putInt(key: String, value: Int) {
     }
 
     actual fun getString(key: String): String? {
-        TODO("Not yet implemented")
+        return ""
     }
 
     actual fun putString(key: String, value: String) {
     }
 
     actual fun getBoolean(key: String): Boolean {
-        TODO("Not yet implemented")
+        return false
     }
 
     actual fun putBoolean(key: String, value: Boolean) {
     }
 
     actual fun getLong(key: String): Long {
-        TODO("Not yet implemented")
+        return 1L
     }
 
     actual fun putLong(key: String, value: Long) {
